b92633fbb8e5afea890997ad552403928e1406b8
Kyle Skinner <kyleskin@gmail.com>
2023-10-19 21:17:47 -0600

    add situation details and team id

--- ScoreTracker.WebApi/NFLTracker/Models/Competition.cs
+++ ScoreTracker.WebApi/NFLTracker/Models/Competition.cs
@@ -5,4 +5,5 @@ public sealed class Competition
     public string Date { get; set; } = string.Empty;
     public Status Status { get; set; } = new();
     public List<Competitor> Competitors { get; set; } = new();
+    public Situation Situation { get; set; } = new();
 }
--- ScoreTracker.WebApi/NFLTracker/Models/Team.cs
+++ ScoreTracker.WebApi/NFLTracker/Models/Team.cs
@@ -2,6 +2,7 @@ namespace ScoreTracker.WebApi.NFLTracker.Models;
 
 public sealed class Team
 {
+    public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Abbreviation { get; set; } = string.Empty;
     public string DisplayName { get; set; } = string.Empty;
