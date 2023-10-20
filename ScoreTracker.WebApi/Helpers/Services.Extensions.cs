04d83afecfa510540787d81079cbcde37cef10e2
Kyle Skinner <kyleskin@gmail.com>
2023-10-19 19:44:15 -0600

    refactor scoreboard services to extension method

--- ScoreTracker.WebApi/Helpers/Services.Extensions.cs
+++ ScoreTracker.WebApi/Helpers/Services.Extensions.cs
@@ -5,6 +5,8 @@ using ScoreTracker.WebApi.NBAScoreTracker.Models;
 using ScoreTracker.WebApi.NBAScoreTracker.Services;
 using ScoreTracker.WebApi.NCAAFootball.Models;
 using ScoreTracker.WebApi.NCAAFootball.Services;
+using ScoreTracker.WebApi.NFL.Models;
+using ScoreTracker.WebApi.NFL.Services;
 using ScoreTracker.WebApi.NHLScoreTracker.Models;
 using ScoreTracker.WebApi.NHLScoreTracker.Services;
 
@@ -15,6 +17,7 @@ public static class Services_Extensions
     public static void AddHttpClients(this IServiceCollection services)
     {
         const string espnBaseSite = "http://site.api.espn.com";
+        
         
         services.AddHttpClient<IScoreboardService<MLBScoreboard>, MLBScoreboardService>((client) =>
         {
@@ -30,10 +33,24 @@ public static class Services_Extensions
         {
             client.BaseAddress = new Uri(espnBaseSite);
         });
+
+        services.AddHttpClient<IScoreboardService<NFLScoreboard>, NFLScoreboardService>((client) =>
+        {
+            client.BaseAddress = new Uri(espnBaseSite);
+        });
         
         services.AddHttpClient<IScoreboardService<NHLScoreboard>, NHLScoreboardService>((client) =>
         {
             client.BaseAddress = new Uri(espnBaseSite);
         });
+    }
+
+    public static void AddScoreboardServices(this IServiceCollection services)
+    {
+        services.AddScoped<IScoreboardService<MLBScoreboard>, MLBScoreboardService>();
+        services.AddScoped<IScoreboardService<NBAScoreboard>, NBAScoreboardService>();
+        services.AddScoped<IScoreboardService<NCAAFootballScoreboard>, NCAAFootballScoreboardService>();
+        services.AddScoped<IScoreboardService<NFLScoreboard>, NFLScoreboardService>();
+        services.AddScoped<IScoreboardService<NHLScoreboard>, NHLScoreboardService>();
     }
 }
